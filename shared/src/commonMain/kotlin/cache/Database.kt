bb7b3aba60a7de5b2dd77af2c272082d42e3b7fe
JarWarren <wrrn24@gmail.com>
2024-01-17 18:07:11 -0700

    contact database methods

--- shared/src/commonMain/kotlin/cache/Database.kt
+++ shared/src/commonMain/kotlin/cache/Database.kt
@@ -1,6 +1,7 @@
 package cache
 
 import com.funnelmink.crm.FunnelminkCache
+import com.funnelmink.crm.SQLContact
 import models.Contact
 import models.User
 
@@ -29,7 +30,47 @@ internal class Database(databaseDriverFactory: DatabaseDriver) {
         )
     }
 
+    fun selectContact(id: String): Contact? {
+        val cached = contactDB.selectContactById(id).executeAsOneOrNull() ?: return null
+        return mapContact(
+            cached.id,
+            cached.firstName,
+            cached.lastName,
+            cached.emails,
+            cached.phoneNumbers,
+            cached.companyName
+        )
+    }
+
+    fun selectAllContacts(): List<Contact> {
+        return contactDB.selectAllContactsInfo(::mapContact).executeAsList()
+    }
     
+    fun updateContact(contact: Contact) {
+        contactDB.updateContact(
+            contact.firstName,
+            contact.lastName,
+            contact.emails.joinToString(separator = ","),
+            contact.phoneNumbers.joinToString(separator = ","),
+            contact.companyName,
+            contact.id
+        )
+    }
+
+    fun deleteAllContacts() {
+        contactDB.removeAllContacts()
+    }
+
+    private fun mapContact(id: String, firstName: String, lastName: String?, emails: String, phoneNumbers: String, companyName: String?): Contact {
+        return Contact(
+            id,
+            firstName,
+            lastName,
+            emails.takeIf { it.isNotBlank() }?.split(",") ?: emptyList(),
+            phoneNumbers.takeIf { it.isNotBlank() }?.split(",") ?: emptyList(),
+            companyName
+        )
+    }
 
     // ------------------------------------------------------------------------
     // Tasks
@@ -46,21 +87,21 @@ internal class Database(databaseDriverFactory: DatabaseDriver) {
     // ------------------------------------------------------------------------
     // Workspace Members
     // ------------------------------------------------------------------------
-    
+
     internal fun clearDatabase() {
         userDB.transaction {
             userDB.removeAllUsers()
         }
     }
-    
+
     private fun createUser(user: User) {
         // TODO: implement the full database for all types!
     }
-    
+
     internal fun insertUser(user: User) {
         userDB.insertUser(user.id, user.email, user.username)
     }
-    
+
     private fun mapUser(id: String, email: String, username: String): User {
         return User(id, username, email)
     }
