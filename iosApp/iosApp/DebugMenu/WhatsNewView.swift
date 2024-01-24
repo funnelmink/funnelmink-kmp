8eebe95bb27f406652f332d8245eb9a865ed95b5
JarWarren <wrrn24@gmail.com>
2024-01-23 22:58:33 -0700

    update wall and whats new view placeholders

---
+++ iosApp/iosApp/DebugMenu/WhatsNewView.swift
@@ -0,0 +1,19 @@
+//
+//  WhatsNewView.swift
+//  iosApp
+//
+//  Created by Jared Warren on 1/23/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct WhatsNewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WhatsNewView()
+}
