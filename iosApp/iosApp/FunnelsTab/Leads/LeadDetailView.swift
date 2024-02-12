daba7a1c83a2876a53cdea8b3035d7c52ba152e1
JarWarren <wrrn24@gmail.com>
2024-02-12 12:54:14 -0700

    delete lead from detail view

--- iosApp/iosApp/FunnelsTab/Leads/LeadDetailView.swift
+++ iosApp/iosApp/FunnelsTab/Leads/LeadDetailView.swift
@@ -16,71 +16,94 @@ struct LeadDetailView: View {
     @State var funnel: Funnel
     @State var stage: FunnelStage
     var body: some View {
-        List {
-            if lead.email != nil || lead.phone != nil || lead.company != nil || lead.source != nil || lead.jobTitle != nil {
-                Section("CONTACT INFORMATION") {
-                    if let email = lead.email {
-                        labeledRow(name: "Email", value: email)
+        VStack {
+            List {
+                if lead.email != nil || lead.phone != nil || lead.company != nil || lead.source != nil || lead.jobTitle != nil {
+                    Section("CONTACT INFORMATION") {
+                        if let email = lead.email {
+                            labeledRow(name: "Email", value: email)
+                        }
+                        if let phone = lead.phone {
+                            labeledRow(name: "Phone", value: phone)
+                        }
+                        if let company = lead.company {
+                            labeledRow(name: "Company", value: company)
+                        }
+                        if let source = lead.source {
+                            labeledRow(name: "Source", value: source)
+                        }
+                        if let jobTitle = lead.jobTitle {
+                            labeledRow(name: "Job Title", value: jobTitle)
+                        }
                     }
-                    if let phone = lead.phone {
-                        labeledRow(name: "Phone", value: phone)
+                }
+                
+                if lead.address != nil || lead.city != nil || lead.state != nil || lead.zip != nil || lead.country != nil {
+                    Section("LOCATION INFORMATION") {
+                        if let address = lead.address {
+                            labeledRow(name: "Address", value: address)
+                        }
+                        if let city = lead.city {
+                            labeledRow(name: "City", value: city)
+                        }
+                        if let state = lead.state {
+                            labeledRow(name: "State", value: state)
+                        }
+                        if let zip = lead.zip {
+                            labeledRow(name: "Zip", value: zip)
+                        }
+                        if let country = lead.country {
+                            labeledRow(name: "Country", value: country)
+                        }
                     }
-                    if let company = lead.company {
-                        labeledRow(name: "Company", value: company)
+                }
+                
+                if let latitude = lead.latitude, let longitude = lead.longitude {
+                    Section("GEO LOCATION") {
+                        labeledRow(name: "Latitude", value: "\(latitude.doubleValue)")
+                        labeledRow(name: "Longitude", value: "\(longitude.doubleValue)")
                     }
-                    if let source = lead.source {
-                        labeledRow(name: "Source", value: source)
+                }
+                
+                Section("LEAD MANAGEMENT") {
+                    if let assignedTo = lead.assignedTo {
+                        labeledRow(name: "Assigned To", value: assignedTo)
                     }
-                    if let jobTitle = lead.jobTitle {
-                        labeledRow(name: "Job Title", value: jobTitle)
+                    // TODO: priority Label
+                    labeledRow(name: "Priority", value: "\(lead.priority)")
+                    labeledRow(name: "Funnel", value: funnel.name)
+                    labeledRow(name: "Stage", value: stage.name)
+                }
+                
+                if let notes = lead.notes {
+                    Section("NOTES") {
+                        Text(notes)
                     }
                 }
             }
-            
-            if lead.address != nil || lead.city != nil || lead.state != nil || lead.zip != nil || lead.country != nil {
-                Section("LOCATION INFORMATION") {
-                    if let address = lead.address {
-                        labeledRow(name: "Address", value: address)
+            HStack {
+                WarningAlertButton(warningMessage: "Are you sure you want to delete this lead?") {
+                    Task {
+                        do {
+                            try await Networking.api.deleteLead(id: lead.id)
+                            navigation.popSegue()
+                        } catch {
+                            Toast.warn(error)
+                        }
                     }
-                    if let city = lead.city {
-                        labeledRow(name: "City", value: city)
-                    }
-                    if let state = lead.state {
-                        labeledRow(name: "State", value: state)
-                    }
-                    if let zip = lead.zip {
-                        labeledRow(name: "Zip", value: zip)
-                    }
-                    if let country = lead.country {
-                        labeledRow(name: "Country", value: country)
-                    }
+                } label: {
+                    Text("Delete Lead")
+                        .foregroundStyle(.red)
+                }
+                Spacer()
+                Button("Convert or Close Lead") {
+                    // TODO: presentation
+                    // convert to account
+                    // gives the option to also create an opportunity
+                    // OR you can close lead
                 }
             }
-            
-            if let latitude = lead.latitude, let longitude = lead.longitude {
-                Section("GEO LOCATION") {
-                    labeledRow(name: "Latitude", value: "\(latitude.doubleValue)")
-                    labeledRow(name: "Longitude", value: "\(longitude.doubleValue)")
-                }
-            }
-            
-            Section("LEAD MANAGEMENT") {
-                if let assignedTo = lead.assignedTo {
-                    labeledRow(name: "Assigned To", value: assignedTo)
-                }
-                // TODO: priority Label
-                labeledRow(name: "Priority", value: "\(lead.priority)")
-                labeledRow(name: "Funnel", value: funnel.name)
-                labeledRow(name: "Stage", value: stage.name)
-            }
-            
-            if let notes = lead.notes {
-                Section("NOTES") {
-                    Text(notes)
-                }
-            }
-            
-            // TODO: Delete Lead and Convert Lead
+            .padding()
         }
         .navigationTitle(lead.name)
         .navigationBarTitleDisplayMode(.inline)
@@ -103,6 +126,5 @@ struct LeadDetailView: View {
             Spacer()
             Text(value)
         }
-//        .discreteListRowStyle()
     }
 }
